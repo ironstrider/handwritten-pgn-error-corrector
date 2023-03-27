3fa6b0b68ffaae597c044c61d11ba18424517c31
Ben Martin <1350682+ironstrider@users.noreply.github.com>
2023-03-27 17:07:34 +1000

    Add move selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import { Component, createSignal, For } from 'solid-js';
 
+import { Chess } from "chess.js";
+
+function checkMoves(moves: string[]) {
+  let chess = new Chess();
+
+  let result = [];
+
+  for (let [ply, move] of moves.entries()) {
+    try {
+      result.push(chess.move(move));
+    } catch (e) {
+      // invalid move
+      break;
+    }
+  }
+
+  return result;
+}
+
 const App: Component = () => {
+  let initialFen = (new Chess()).fen();
+  let [fen, setFen] = createSignal(initialFen);
   let [moves, setMoves] = createSignal([] as string[]);
 
   let addMoves = (moveText: string) => {
-    setMoves(moves => [...moves, ...moveText.split(/\s+/g)])
+    setMoves(moves => {
+      let newMoves = [...moves, ...moveText.split(/\s+/g)];
+
+      console.log('valid moves', checkMoves(newMoves));
+
+      return newMoves;
+    })
+  }
+
+  let selectMove = (ply: number) => {
+    let validMoves = checkMoves(moves());
+    setFen(validMoves[ply].after)
   }
 
   return (
     <div class="dislay: grid; grid-template-columns: auto;">
+      <code>Fen: {fen()}</code>
       <div style="display: grid; grid-template-columns: auto auto auto; grid-column-gap: 2em;">
+        <span></span>
+        <span onClick={() => { setFen(initialFen) }} style="grid-column: span 2"><em>Start</em></span>
         <For each={moves()}>
           {(move, index) => {
 
             let i = index();
 
             if (i % 2 === 1) {
-              return <span>{move}</span>
+              return <span onClick={() => selectMove(i)}>{move}</span>
             } else {
               return <>
                 <span>{Math.floor(i / 2 + 1)}. </span>
-                <span>{move}</span>
+                <span onClick={() => selectMove(i)}>{move}</span>
               </>
             }
 
